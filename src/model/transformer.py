f830b913be89225e4bb68ccb8b7da881a26b3061
nsapru@uoguelph.ca <nsapru@ws13>
2018-09-26 18:29:20 -0400

    Adding training file and updated transformer

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -360,3 +360,24 @@ class LabelSmoothing(nn.Module):
         self.true_dist = true_dist
         return self.criterion(x, Variable(true_dist, requires_grad=False))
 
+
+def greedy_decode(model, src, src_mask, max_len, start_symbol):
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
+    for i in range(max_len-1):
+        out = model.decode(memory, src_mask, 
+                           Variable(ys), 
+                           Variable(subsequent_mask(ys.size(1))
+                                    .type_as(src.data)))
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim = 1)
+        next_word = next_word.data[0]
+        ys = torch.cat([ys, 
+                        torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+    return ys
+
+#model.eval()
+#src = Variable(torch.LongTensor([[1,2,3,4,5,6,7,8,9,10]]) )
+#src_mask = Variable(torch.ones(1, 1, 10) )
+#print(greedy_decode(model, src, src_mask, max_len=10, start_symbol=1))
+
