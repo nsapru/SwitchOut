f25d1d2e6f2e866c3b34da6dd0304dd8eb2d8c0c
Nikhil Sapru <nikhil.sapru@gmail.com>
2018-10-08 19:44:53 -0700

    Added helper function for batching with torchtext

--- train.py
+++ train.py
@@ -52,8 +52,10 @@ TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
 
 
 ##########################################################################
-#######################   BATCHING & MASKING   ###########################
+##################   OBJECT FOR BATCHES & MASKING   ######################
 ##########################################################################
+
+
 class Batch:
     "Object for holding a batch of data with mask during training."
     def __init__(self, src, trg=None, pad=0):
@@ -74,3 +76,21 @@ class Batch:
             transformer.subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
         return tgt_mask
 
+
+##########################################################################
+####################   CREATE BATCHES WITH TORCHTEXT   ###################
+##########################################################################
+
+
+global max_src_in_batch, max_tgt_in_batch
+def batch_size_fn(new, count, sofar):
+    "Keep augmenting batch and calculate total number of tokens + padding."
+    global max_src_in_batch, max_tgt_in_batch
+    if count == 1:
+        max_src_in_batch = 0
+        max_tgt_in_batch = 0
+    max_src_in_batch = max(max_src_in_batch,  len(new.src))
+    max_tgt_in_batch = max(max_tgt_in_batch,  len(new.trg) + 2)
+    src_elements = count * max_src_in_batch
+    tgt_elements = count * max_tgt_in_batch
+    return max(src_elements, tgt_elements)
