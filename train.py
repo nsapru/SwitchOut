7566ca79456380814de279df053b834331d7ff30
Nikhil Sapru <nikhil.sapru@gmail.com>
2018-10-08 19:09:06 -0700

    Adding class to batch and mask data

--- train.py
+++ train.py
@@ -48,3 +48,29 @@ train, val, test = datasets.IWSLT.splits(
 MIN_FREQ = 10000
 SRC.build_vocab(train.src, min_freq=MIN_FREQ)
 TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+
+
+
+##########################################################################
+#######################   BATCHING & MASKING   ###########################
+##########################################################################
+class Batch:
+    "Object for holding a batch of data with mask during training."
+    def __init__(self, src, trg=None, pad=0):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-2)
+        if trg is not None:
+            self.trg = trg[:, :-1]
+            self.trg_y = trg[:, 1:]
+            self.trg_mask = \
+                self.make_std_mask(self.trg, pad)
+            self.ntokens = (self.trg_y != pad).data.sum()
+    
+    @staticmethod
+    def make_std_mask(tgt, pad):
+        "Create a mask to hide padding and future words."
+        tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = tgt_mask & Variable(
+            transformer.subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+        return tgt_mask
+
